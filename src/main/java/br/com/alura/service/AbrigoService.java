8f8d4a218f6cf8bdedc989b1597c962af7e45b68
Eduardo Martins <edu.almeida.01@hotmail.com>
2024-01-31 23:14:57 -0300

    Removing imports.

--- src/main/java/br/com/alura/service/AbrigoService.java
+++ src/main/java/br/com/alura/service/AbrigoService.java
@@ -7,9 +7,6 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
--- src/main/java/br/com/alura/service/PetService.java
+++ src/main/java/br/com/alura/service/PetService.java
@@ -9,9 +9,6 @@ import com.google.gson.JsonParser;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
